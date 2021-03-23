016c7059173af087905ac03af82f52ca3bccb512
Cleverttech <dzifatedeku@gmail.com>
2021-03-23 12:11:07 +0100

    add objectand manipulation

--- object-array.js
+++ object-array.js
@@ -29,6 +29,42 @@ log(Object.keys(ironhacker));
 
 
 // 6: check if ironhacker has property 'works at'
+log(['works at'] in ironhacker); 
+
+
+
+//ARRAYS
+
+const ironhacker = {
+    firstName: 'marko',
+    age: 39,
+    favoriteLanguage: 'JavaScript',
+    isSatisfied: true,
+    'works at': 'Google',
+    isRemote: false
+}
+
+// 1: update favoriteLanguage to 'React' 
+ironhacker.favoriteLanguage = 'React'
+log(ironhacker.favoriteLanguage)
+
+// 2: Update ironhacker's age to 29
+ ironhacker.age = 29;
+log(ironhacker.age)
+
+// 3: Remove the 'isRemote' property
+delete ironhacker.isRemote;
+log(ironhacker)
+
+// 4: Add a new property: 'didGraduate' and set it to a valid boolean value
+ironhacker.didGraduate = true;
+log(ironhacker)
+
+// 5: list all keys of the ironhacker object
+log(Object.keys(ironhacker));
+
+
+// 6: check if ironhacker has property 'works at'
 log(['works at'] in ironhacker);
 
 //ARRAYS
